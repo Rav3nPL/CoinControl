705672372a88fc95dbf74939b2b2ec09240117da
Rav3nPL <rav3.pl@gmail.com>
2015-01-29 08:39:58 +0100

    Initial commit

---
+++ main.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace CoinControl
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btGet = new System.Windows.Forms.Button();
+            this.btSign = new System.Windows.Forms.Button();
+            this.btSend = new System.Windows.Forms.Button();
+            this.dgvIn = new System.Windows.Forms.DataGridView();
+            this.take = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.amount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.confirmations = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txid = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.vout = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tbSent = new System.Windows.Forms.TextBox();
+            this.tbFee = new System.Windows.Forms.TextBox();
+            this.tbOut = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dgvOut = new System.Windows.Forms.DataGridView();
+            this.btPrepare = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btConnect = new System.Windows.Forms.Button();
+            this.tbPass = new System.Windows.Forms.TextBox();
+            this.tbUser = new System.Windows.Forms.TextBox();
+            this.tbIp = new System.Windows.Forms.TextBox();
+            this.tbLog = new System.Windows.Forms.TextBox();
+            this.Address = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.outAmount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvIn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOut)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btGet
+            // 
+            this.btGet.Location = new System.Drawing.Point(12, 12);
+            this.btGet.Name = "btGet";
+            this.btGet.Size = new System.Drawing.Size(75, 23);
+            this.btGet.TabIndex = 0;
+            this.btGet.Text = "Get inputs";
+            this.btGet.UseVisualStyleBackColor = true;
+            this.btGet.Click += new System.EventHandler(this.btGet_Click);
+            // 
+            // btSign
+            // 
+            this.btSign.Location = new System.Drawing.Point(690, 373);
+            this.btSign.Name = "btSign";
+            this.btSign.Size = new System.Drawing.Size(75, 23);
+            this.btSign.TabIndex = 1;
+            this.btSign.Text = "Sign TX";
+            this.btSign.UseVisualStyleBackColor = true;
+            this.btSign.Click += new System.EventHandler(this.btSign_Click);
+            // 
+            // btSend
+            // 
+            this.btSend.Location = new System.Drawing.Point(771, 373);
+            this.btSend.Name = "btSend";
+            this.btSend.Size = new System.Drawing.Size(75, 23);
+            this.btSend.TabIndex = 2;
+            this.btSend.Text = "Send TX";
+            this.btSend.UseVisualStyleBackColor = true;
+            this.btSend.Click += new System.EventHandler(this.btSend_Click);
+            // 
+            // dgvIn
+            // 
+            this.dgvIn.AllowUserToAddRows = false;
+            this.dgvIn.AllowUserToDeleteRows = false;
+            this.dgvIn.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvIn.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.take,
+            this.amount,
+            this.confirmations,
+            this.txid,
+            this.vout});
+            this.dgvIn.Location = new System.Drawing.Point(12, 41);
+            this.dgvIn.Name = "dgvIn";
+            this.dgvIn.Size = new System.Drawing.Size(834, 221);
+            this.dgvIn.TabIndex = 3;
+            this.dgvIn.CellMouseUp += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvIn_CellMouseUp);
+            this.dgvIn.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvIn_CellValueChanged);
+            // 
+            // take
+            // 
+            this.take.HeaderText = "take";
+            this.take.Name = "take";
+            // 
+            // amount
+            // 
+            this.amount.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.amount.HeaderText = "amount";
+            this.amount.Name = "amount";
+            this.amount.ReadOnly = true;
+            this.amount.Width = 67;
+            // 
+            // confirmations
+            // 
+            this.confirmations.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.confirmations.HeaderText = "confirmations";
+            this.confirmations.Name = "confirmations";
+            this.confirmations.ReadOnly = true;
+            this.confirmations.Width = 94;
+            // 
+            // txid
+            // 
+            this.txid.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.txid.HeaderText = "TX ID";
+            this.txid.Name = "txid";
+            this.txid.ReadOnly = true;
+            this.txid.Width = 60;
+            // 
+            // vout
+            // 
+            this.vout.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.vout.HeaderText = "vout";
+            this.vout.Name = "vout";
+            this.vout.ReadOnly = true;
+            this.vout.Width = 53;
+            // 
+            // tbSent
+            // 
+            this.tbSent.Location = new System.Drawing.Point(12, 376);
+            this.tbSent.Name = "tbSent";
+            this.tbSent.Size = new System.Drawing.Size(100, 20);
+            this.tbSent.TabIndex = 4;
+            this.tbSent.Text = "0";
+            // 
+            // tbFee
+            // 
+            this.tbFee.Location = new System.Drawing.Point(118, 376);
+            this.tbFee.Name = "tbFee";
+            this.tbFee.Size = new System.Drawing.Size(100, 20);
+            this.tbFee.TabIndex = 5;
+            this.tbFee.Text = "0";
+            // 
+            // tbOut
+            // 
+            this.tbOut.Location = new System.Drawing.Point(224, 376);
+            this.tbOut.Name = "tbOut";
+            this.tbOut.Size = new System.Drawing.Size(100, 20);
+            this.tbOut.TabIndex = 6;
+            this.tbOut.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 360);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Total INs";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 360);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Fee";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(221, 360);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Total OUTs";
+            // 
+            // dgvOut
+            // 
+            this.dgvOut.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOut.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Address,
+            this.outAmount});
+            this.dgvOut.Location = new System.Drawing.Point(12, 268);
+            this.dgvOut.Name = "dgvOut";
+            this.dgvOut.Size = new System.Drawing.Size(834, 79);
+            this.dgvOut.TabIndex = 10;
+            this.dgvOut.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvOut_CellValueChanged);
+            // 
+            // btPrepare
+            // 
+            this.btPrepare.Location = new System.Drawing.Point(609, 373);
+            this.btPrepare.Name = "btPrepare";
+            this.btPrepare.Size = new System.Drawing.Size(75, 23);
+            this.btPrepare.TabIndex = 11;
+            this.btPrepare.Text = "Prepare TX";
+            this.btPrepare.UseVisualStyleBackColor = true;
+            this.btPrepare.Click += new System.EventHandler(this.btPrepare_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btConnect);
+            this.panel1.Controls.Add(this.tbPass);
+            this.panel1.Controls.Add(this.tbUser);
+            this.panel1.Controls.Add(this.tbIp);
+            this.panel1.Location = new System.Drawing.Point(268, 108);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(254, 103);
+            this.panel1.TabIndex = 12;
+            // 
+            // btConnect
+            // 
+            this.btConnect.Location = new System.Drawing.Point(176, 36);
+            this.btConnect.Name = "btConnect";
+            this.btConnect.Size = new System.Drawing.Size(75, 23);
+            this.btConnect.TabIndex = 7;
+            this.btConnect.Text = "Connect!";
+            this.btConnect.UseVisualStyleBackColor = true;
+            this.btConnect.Click += new System.EventHandler(this.btConnect_Click);
+            // 
+            // tbPass
+            // 
+            this.tbPass.Location = new System.Drawing.Point(17, 64);
+            this.tbPass.Name = "tbPass";
+            this.tbPass.Size = new System.Drawing.Size(151, 20);
+            this.tbPass.TabIndex = 6;
+            this.tbPass.Text = "rpcpassword";
+            // 
+            // tbUser
+            // 
+            this.tbUser.Location = new System.Drawing.Point(17, 38);
+            this.tbUser.Name = "tbUser";
+            this.tbUser.Size = new System.Drawing.Size(151, 20);
+            this.tbUser.TabIndex = 5;
+            this.tbUser.Text = "rpcuser";
+            // 
+            // tbIp
+            // 
+            this.tbIp.Location = new System.Drawing.Point(17, 12);
+            this.tbIp.Name = "tbIp";
+            this.tbIp.Size = new System.Drawing.Size(151, 20);
+            this.tbIp.TabIndex = 4;
+            this.tbIp.Text = "ip:port";
+            // 
+            // tbLog
+            // 
+            this.tbLog.Location = new System.Drawing.Point(12, 402);
+            this.tbLog.Multiline = true;
+            this.tbLog.Name = "tbLog";
+            this.tbLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbLog.Size = new System.Drawing.Size(834, 132);
+            this.tbLog.TabIndex = 13;
+            // 
+            // Address
+            // 
+            this.Address.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Address.HeaderText = "Address";
+            this.Address.MaxInputLength = 50;
+            this.Address.MinimumWidth = 100;
+            this.Address.Name = "Address";
+            // 
+            // outAmount
+            // 
+            this.outAmount.HeaderText = "amount";
+            this.outAmount.Name = "outAmount";
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(858, 546);
+            this.Controls.Add(this.tbLog);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btPrepare);
+            this.Controls.Add(this.dgvOut);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbOut);
+            this.Controls.Add(this.tbFee);
+            this.Controls.Add(this.tbSent);
+            this.Controls.Add(this.dgvIn);
+            this.Controls.Add(this.btSend);
+            this.Controls.Add(this.btSign);
+            this.Controls.Add(this.btGet);
+            this.Name = "main";
+            this.Text = "Rav3n_pl RPC Coin Control for any coin";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvIn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOut)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btGet;
+        private System.Windows.Forms.Button btSign;
+        private System.Windows.Forms.Button btSend;
+        private System.Windows.Forms.DataGridView dgvIn;
+        private System.Windows.Forms.TextBox tbSent;
+        private System.Windows.Forms.TextBox tbFee;
+        private System.Windows.Forms.TextBox tbOut;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dgvOut;
+        private System.Windows.Forms.Button btPrepare;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btConnect;
+        private System.Windows.Forms.TextBox tbPass;
+        private System.Windows.Forms.TextBox tbUser;
+        private System.Windows.Forms.TextBox tbIp;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn take;
+        private System.Windows.Forms.DataGridViewTextBoxColumn amount;
+        private System.Windows.Forms.DataGridViewTextBoxColumn confirmations;
+        private System.Windows.Forms.DataGridViewTextBoxColumn txid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn vout;
+        private System.Windows.Forms.TextBox tbLog;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Address;
+        private System.Windows.Forms.DataGridViewTextBoxColumn outAmount;
+    }
+}
---
+++ main.cs
@@ -0,0 +1,279 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace CoinControl
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+            dgvOut.Rows.Add("1Rav3nkMayCijuhzcYemMiPYsvcaiwHni", "0.01");
+            log("Fill IP:port, rpcuser and rpcpassword, press button.");
+            log("Small suggestion in outputs... :D");
+        }
+        public string ip;
+        public string rpcuser;
+        public string rpcpassword;
+        public string hextx = "";
+        public string signtx = "";
+
+        public string SendCommand(string strCommand, string strParameters = "") //from namecoinGUI
+        {
+            byte[] byteResponse = new byte[0];
+            string strResponse = string.Empty;
+
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(ip);
+
+            webRequest.Credentials = new NetworkCredential(rpcuser, rpcpassword);
+            webRequest.ContentType = "application/json-rpc";
+            webRequest.Method = "POST";
+
+            string s = "{\"jsonrpc\": \"1.0\", \"id\":\"Rav3nCoinControl\", \"method\": \"" + strCommand + "\", \"params\":[" + strParameters + "]}";
+            byte[] byteArray = Encoding.UTF8.GetBytes(s);
+            log(s);
+            webRequest.ContentLength = byteArray.Length;
+            try
+            {
+                Stream dataStream = webRequest.GetRequestStream();
+                dataStream.Write(byteArray, 0, byteArray.Length);
+                dataStream.Close();
+
+                WebResponse webResponse = webRequest.GetResponse();
+
+                if (webResponse.ContentLength > 0)
+                {
+                    StreamReader ResponseStream = new StreamReader(webResponse.GetResponseStream());
+                    strResponse = ResponseStream.ReadToEnd();
+                    ResponseStream.Close();
+                }
+                webResponse.Close();
+            }
+            catch (System.Net.WebException e)
+            {
+                    string strLog = DateTime.Now.ToString() + ": " + e.Message + Environment.NewLine;
+                    log(strLog);
+                    MessageBox.Show(strLog, "Communication Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    if (strResponse != "") { log(strResponse); }
+            }
+
+            if (strResponse != string.Empty)
+            {
+                log(strResponse);
+            }
+            return strResponse;
+        }
+
+
+        private void btGet_Click(object sender, EventArgs e)
+        {
+            tbLog.Text = "";
+            string resp = SendCommand("listunspent");
+            log("Loading inputs, it might take a while...");
+            btGet.Enabled = false;
+            Application.DoEvents();
+            if (resp != "")
+            {
+                dynamic d = JsonConvert.DeserializeObject(resp);
+                dgvIn.Rows.Clear();
+                foreach (dynamic i in d.result)
+                {
+                    dgvIn.Rows.Add(false, i.amount, i.confirmations, i.txid, i.vout);
+                }
+                log("Data loaded. Sort inputs and choose!");
+            }
+            else
+            {
+                log("Oops!");
+            }
+            btGet.Enabled = true;
+        }
+        
+            
+        void log(string s)
+        {
+            tbLog.Text = s + Environment.NewLine + tbLog.Text;
+        }
+        private void btConnect_Click(object sender, EventArgs e)
+        {
+            ip = "http://" + tbIp.Text;
+            rpcuser = tbUser.Text;
+            rpcpassword = tbPass.Text;
+            string resp = "";
+            try
+            {
+                resp = SendCommand("getinfo");
+            }
+            catch (Exception ex)
+            {
+                log("Connection error!");
+                log(ex.Message.ToString());
+            }
+            finally
+            {
+                if (resp != "")
+                {
+                    log(resp);
+                    log("Connected, now get inputs.");
+                    panel1.Visible = false;
+                }
+            }
+        }
+    
+        private void btPrepare_Click(object sender, EventArgs e)
+        {
+            //przygotuj nie podpisaną tx do sprawdzenia: createrawtransaction [{"txid":txid,"vout":n},...] {address:amount,...}
+            if (dgvOut.Rows[0].Cells[0].Value != null)
+            {
+                btPrepare.Enabled = false;
+                log("Gathering data...");
+                Application.DoEvents();
+
+                string i = " [";
+                //zbieramy inputy
+                foreach (DataGridViewRow d in dgvIn.Rows)
+                {
+                    if (Convert.ToBoolean(d.Cells[0].Value) == true)
+                    {
+                        string tx = d.Cells[3].Value.ToString();
+                        string vout = d.Cells[4].Value.ToString();
+                        i += "{\"txid\":\"" + tx + "\",\"vout\":" + vout + "},";
+                    }
+                }
+                i = i.Remove(i.Length - 1); //kill last ","
+                i += "], {";
+
+                //zbieramy outputy
+                foreach (DataGridViewRow d in dgvOut.Rows)
+                {
+                    if (d.Cells[0].Value != null)
+                    {
+                        string addr = d.Cells[0].Value.ToString();
+                        double am = Convert.ToDouble(d.Cells[1].Value.ToString());
+                        string amou = String.Format(CultureInfo.InvariantCulture, "{0:0.00000000}", am);
+                        i += "\""+addr +"\":" + amou + ",";
+                    }
+                }
+                i = i.Remove(i.Length - 1); //kill last ","
+                i += "} ";
+                log("tx:");
+                log(i);
+                log("Creating transaction on daemon, please be patient...");
+                Application.DoEvents();
+                string resp = SendCommand("createrawtransaction", i);
+                if (resp != "")
+                {
+                    dynamic dy = JsonConvert.DeserializeObject(resp);
+                    hextx = dy.result;
+                    log(resp);
+                    log("Transaction created, now sign it!");
+                }
+                else
+                {
+                    log("Error! Check addresses, amounts. Maybe bigger fee need?");
+                }
+                btPrepare.Enabled = true;
+            }
+            else
+            {
+                log("No output address data!");
+            }
+        }
+        
+
+        private void btSign_Click(object sender, EventArgs e)
+        {
+            //wyślij do podpisu: signrawtransaction <hexstring> [{"txid":txid,"vout":n,"scriptPubKey":hex},...] [<privatekey1>,...]
+            log("Sending TX to sign by daemon...");
+            btSign.Enabled = false;
+            Application.DoEvents();
+            string resp = SendCommand("signrawtransaction", "\"" + hextx + "\"");
+            if (resp != "")
+            {
+                dynamic dy = JsonConvert.DeserializeObject(resp);
+                signtx = dy.result.hex;
+                log("Transaction signed, now send it!");
+            }
+            else
+            {
+                log("Oops! Try again?");
+            }
+            btSign.Enabled = true;
+        }
+
+        private void btSend_Click(object sender, EventArgs e)
+        {
+            //wyślij podpisaną sendrawtransaction <hexstring>
+            log("Sending TX to network...");
+            btSend.Enabled = false;
+            Application.DoEvents();
+            string resp = SendCommand("sendrawtransaction", "\"" + signtx + "\"");
+            if (resp != "")
+            {
+                dynamic dy = JsonConvert.DeserializeObject(resp);
+                string txid = dy.result;
+                log("Transaction sent, TXID: " + txid);
+                log("Clearing input list.");
+                dgvIn.Rows.Clear();
+            }
+            else
+            {
+                log("Oops! Try again?");
+            }
+            btSend.Enabled = true;
+        }
+
+        private void dgvIn_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            //obsługa klika na checkbox
+            if (e.ColumnIndex == 0 && e.RowIndex != -1)
+            {
+                double amount = 0;
+                foreach (DataGridViewRow d in dgvIn.Rows)
+                {
+                    if (Convert.ToBoolean(d.Cells[0].Value)==true)
+                    {
+                        amount += Convert.ToDouble(d.Cells[1].Value);
+                    }
+                }
+                tbSent.Text = amount.ToString();
+                dgvOut.Rows[0].Cells[1].Value = amount;
+            }
+        }
+
+
+        private void dgvIn_CellMouseUp(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            //po kliku odpal koniec edycji
+            if (e.ColumnIndex == 0 && e.RowIndex != -1)
+            {
+                dgvIn.EndEdit();
+            }
+        }
+
+        private void dgvOut_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            //zmiana danych
+            double amount = 0;
+            foreach (DataGridViewRow d in dgvOut.Rows)
+            {
+                if (d.Cells[1].Value != null)
+                {
+                    double add = 0;
+                    string s = d.Cells[1].Value.ToString();
+                    Double.TryParse(s, out add);
+                    if (add > 0) { amount += add; }
+                    else { log("Incorrect value in output?"); }
+                }
+            }
+            tbOut.Text = amount.ToString();
+            tbFee.Text = String.Format(CultureInfo.InvariantCulture, "{0:0.00000000}", (Convert.ToDouble(tbSent.Text) - Convert.ToDouble(tbOut.Text)).ToString());
+        }
+    }
+}
